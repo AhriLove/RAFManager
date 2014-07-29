c0de0c2ece43250bf2708dfd85389e8022a03368
Yannic Hamann <hamann@idicos.de>
2014-07-29 14:46:27 +0200

    Increased performance and complete extraction of all .raf.dat files

--- RAFManager/Program.cs
+++ RAFManager/Program.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using zlib = ComponentAce.Compression.Libs.zlib;
-using System.Linq;
 
 // http://leagueoflegends.wikia.com/wiki/RAF:_Riot_Archive_File
 namespace RAFManager
@@ -33,6 +31,7 @@ namespace RAFManager
             public byte[] DataOffset;
             public byte[] DataSize;
             public byte[] PathListIndex;
+            public byte[] Buffer; // Placeholder for content from .raf.dat file
         }
 
         public class RafPathList
@@ -51,112 +50,88 @@ namespace RAFManager
 
         static void Main(string[] args)
         {
-            //const string pathRaf = @"C:\Riot Games\League of Legends\RADS\projects\lol_game_client\filearchives\0.0.0.25\Archive_2.raf";
-            const string pathRaf = @"D:\League of Legends\RADS\projects\lol_game_client\filearchives\0.0.0.25\Archive_2.raf";
-            const string pathRafDat = @"D:\League of Legends\RADS\projects\lol_game_client\filearchives\0.0.0.25\Archive_2.raf.dat";
+            const string filearchiveDir = @"C:\Riot Games\League of Legends\RADS\projects\lol_game_client\filearchives";
+            string[] rafFiles = Directory.GetFiles(filearchiveDir, "*.raf", SearchOption.AllDirectories);
 
-            using (var binaryReaderRaf = new BinaryReader(File.OpenRead(pathRaf)))
+            foreach (string rafFilePath in rafFiles)
             {
-                using (var binaryReadRafDat = new BinaryReader(File.OpenRead(pathRafDat)))
+                using (var binaryReaderRaf = new BinaryReader(File.OpenRead(rafFilePath)))
                 {
-                    RafFileHeader rafFileHeader = new RafFileHeader
+                    using (var binaryReadRafDat = new BinaryReader(File.OpenRead(Path.ChangeExtension(rafFilePath, ".raf.dat"))))
                     {
-                        MagicNumber = binaryReaderRaf.ReadBytes(4),
-                        Version = binaryReaderRaf.ReadBytes(4),
-                        ManagerIndex = binaryReaderRaf.ReadBytes(4),
-                        FileListOffset = binaryReaderRaf.ReadBytes(4),
-                        PathListOffset = binaryReaderRaf.ReadBytes(4)
-                    };
-
-                    binaryReaderRaf.BaseStream.Position = BitConverter.ToInt32(rafFileHeader.FileListOffset, 0);
-                    var rafFileList = new RafFileList
-                    {
-                        NumberOfEntries = binaryReaderRaf.ReadBytes(4)
-                    };
-                    rafFileHeader.FileList = rafFileList;
-                    rafFileList.Entries = new List<RafFileListEntry>();
-                    for (int i = 0; i < BitConverter.ToInt32(rafFileList.NumberOfEntries, 0); i++)
-                    {
-                        var fileListEntry = new RafFileListEntry
+                        var rafFileHeader = new RafFileHeader
                         {
-                            PathHash = binaryReaderRaf.ReadBytes(4),
-                            DataOffset = binaryReaderRaf.ReadBytes(4),
-                            DataSize = binaryReaderRaf.ReadBytes(4),
-                            PathListIndex = binaryReaderRaf.ReadBytes(4)
+                            MagicNumber = binaryReaderRaf.ReadBytes(4),
+                            Version = binaryReaderRaf.ReadBytes(4),
+                            ManagerIndex = binaryReaderRaf.ReadBytes(4),
+                            FileListOffset = binaryReaderRaf.ReadBytes(4),
+                            PathListOffset = binaryReaderRaf.ReadBytes(4)
                         };
-                        rafFileHeader.FileList.Entries.Add(fileListEntry);
-                    }
 
-                    binaryReaderRaf.BaseStream.Position = BitConverter.ToInt32(rafFileHeader.PathListOffset, 0);
-                    var positionPathList = binaryReaderRaf.BaseStream.Position;
-                    var rafPathList = new RafPathList()
-                    {
-                        PathListSize = binaryReaderRaf.ReadBytes(4),
-                        PathListCount = binaryReaderRaf.ReadBytes(4)
-                    };
-                    rafFileHeader.PathList = rafPathList;
-                    rafPathList.PathListEntries = new List<RafPathListEntry>();
-                    rafPathList.PathStrings = new List<string>();
-
-
-                    for (int i = 0; i < BitConverter.ToInt32(rafPathList.PathListCount, 0); i++)
-                    {
-                        var pathListEntry = new RafPathListEntry
+                        binaryReaderRaf.BaseStream.Position = BitConverter.ToInt32(rafFileHeader.PathListOffset, 0);
+                        var positionPathList = binaryReaderRaf.BaseStream.Position;
+                        var rafPathList = new RafPathList()
                         {
-                            PathOffset = binaryReaderRaf.ReadBytes(4),
-                            PathLength = binaryReaderRaf.ReadBytes(4)
+                            PathListSize = binaryReaderRaf.ReadBytes(4),
+                            PathListCount = binaryReaderRaf.ReadBytes(4)
                         };
-                        rafPathList.PathListEntries.Add(pathListEntry);
-                    }
+                        rafFileHeader.PathList = rafPathList;
+                        rafPathList.PathListEntries = new List<RafPathListEntry>();
+                        rafPathList.PathStrings = new List<string>();
 
-                    foreach (var pathListEntry in rafPathList.PathListEntries)
-                    {
-                        var pathOffset = BitConverter.ToInt32(pathListEntry.PathOffset, 0);
-                        var pathLength = BitConverter.ToInt32(pathListEntry.PathLength, 0);
 
-                        binaryReaderRaf.BaseStream.Position = positionPathList + pathOffset;
-                        rafPathList.PathStrings.Add(Encoding.ASCII.GetString(binaryReaderRaf.ReadBytes(pathLength - 1)));
-                    }
-                    
-                    for (int i = 0; i < rafFileHeader.FileList.Entries.Count; i++)
-                    {
-                        Console.Write("\x000D{0}%   ", ((double)i / (double)rafFileHeader.FileList.Entries.Count * 100).ToString("0.00"));
-                        var fileListEntry = rafFileHeader.FileList.Entries.FirstOrDefault(item => BitConverter.ToInt32(item.PathListIndex, 0) == i);
-
-                        int fileListEntryOffset = BitConverter.ToInt32(fileListEntry.DataOffset, 0);
-                        int fileListEntryDataSize = BitConverter.ToInt32(fileListEntry.DataSize, 0);
-                        binaryReadRafDat.BaseStream.Position = fileListEntryOffset;
-                        byte[] buffer = binaryReadRafDat.ReadBytes(fileListEntryDataSize);
-
-                        string path = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "export", rafFileHeader.PathList.PathStrings[i]));
-                        try
+                        for (int i = 0; i < BitConverter.ToInt32(rafPathList.PathListCount, 0); i++)
                         {
-                            using (MemoryStream mStream = new MemoryStream(buffer))
+                            var pathListEntry = new RafPathListEntry
                             {
-                                using (zlib.ZInputStream zinput = new zlib.ZInputStream(mStream))
-                                {
-                                    List<byte> dBuffer = new List<byte>();
-                                    int data;
-                                    while ((data = zinput.Read()) != -1)
-                                    {
-                                        dBuffer.Add((byte)data);
-                                    }
-                                    buffer = dBuffer.ToArray();
-                                }
-                            }
+                                PathOffset = binaryReaderRaf.ReadBytes(4),
+                                PathLength = binaryReaderRaf.ReadBytes(4)
+                            };
+                            rafPathList.PathListEntries.Add(pathListEntry);
+
+                            long position = binaryReaderRaf.BaseStream.Position; // remember position
+
+                            var pathOffset = BitConverter.ToInt32(pathListEntry.PathOffset, 0);
+                            var pathLength = BitConverter.ToInt32(pathListEntry.PathLength, 0);
+                            binaryReaderRaf.BaseStream.Position = positionPathList + pathOffset;
+                            rafPathList.PathStrings.Add(
+                                Encoding.ASCII.GetString(binaryReaderRaf.ReadBytes(pathLength - 1)));
+
+                            binaryReaderRaf.BaseStream.Position = position; // jump back
                         }
-                        catch (Exception x)
+
+                        binaryReaderRaf.BaseStream.Position = BitConverter.ToInt32(rafFileHeader.FileListOffset, 0);
+                        var rafFileList = new RafFileList
                         {
-                        }
-                        if (!Directory.Exists(Path.GetDirectoryName(path)))
+                            NumberOfEntries = binaryReaderRaf.ReadBytes(4)
+                        };
+                        rafFileHeader.FileList = rafFileList;
+                        rafFileList.Entries = new List<RafFileListEntry>();
+                        for (int i = 0; i < BitConverter.ToInt32(rafFileList.NumberOfEntries, 0); i++)
                         {
-                            Directory.CreateDirectory(Path.GetDirectoryName(path));
+                            Console.Write("\x000D{0}%", (i/(double) BitConverter.ToInt32(rafFileList.NumberOfEntries, 0)*100).ToString("0.00"));
+                            var fileListEntry = new RafFileListEntry
+                            {
+                                PathHash = binaryReaderRaf.ReadBytes(4),
+                                DataOffset = binaryReaderRaf.ReadBytes(4),
+                                DataSize = binaryReaderRaf.ReadBytes(4),
+                                PathListIndex = binaryReaderRaf.ReadBytes(4)
+                            };
+                            rafFileHeader.FileList.Entries.Add(fileListEntry);
+
+                            int fileListEntryOffset = BitConverter.ToInt32(fileListEntry.DataOffset, 0);
+                            int fileListEntryDataSize = BitConverter.ToInt32(fileListEntry.DataSize, 0);
+                            binaryReadRafDat.BaseStream.Position = fileListEntryOffset;
+                            fileListEntry.Buffer = RafDat.Deflate(binaryReadRafDat.ReadBytes(fileListEntryDataSize));
+
+                            string version = new DirectoryInfo(rafFilePath).Parent.Name;
+                            RafDat.Save(fileListEntry.Buffer, Path.Combine(version, rafPathList.PathStrings[BitConverter.ToInt32(fileListEntry.PathListIndex, 0)]));
                         }
-                        File.WriteAllBytes(path, buffer);                                     
                     }
                 }
             }
 
+            Console.WriteLine("Done.");
             Console.ReadKey();
         }
     }
---
+++ RAFManager/RafDat.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace RAFManager
+{
+    public static class RafDat
+    {
+        public static string ApplicationDirectoryPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+        public static void Save(byte[] buffer, string pathString)
+        {
+            try
+            {
+                if (buffer == null)
+                {
+                    throw new NullReferenceException();
+                }
+
+                string filePath = Path.GetFullPath(Path.Combine(ApplicationDirectoryPath, "export", pathString));
+                string directoryFilePath = Path.GetDirectoryName(filePath);
+                if (!Directory.Exists(directoryFilePath))
+                {
+                    Directory.CreateDirectory(directoryFilePath);
+                }
+                File.WriteAllBytes(filePath, buffer);
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(pathString + " - " + x.Message);
+            }
+        }
+
+        public static byte[] Deflate(byte[] buffer, long position = 2)
+        {
+            try
+            {
+                using (var compressedMemoryStream = new MemoryStream(buffer))
+                {
+                    compressedMemoryStream.Position = position; // Skip bytes
+                    using (var deflateStream = new DeflateStream(compressedMemoryStream, CompressionMode.Decompress))
+                    {
+                        using (var uncompressedMemoryStream = new MemoryStream())
+                        {
+                            deflateStream.CopyTo(uncompressedMemoryStream);
+                            return uncompressedMemoryStream.ToArray();
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return position == 2 ? Deflate(buffer, 0) : null;
+            }
+        }
+    }
+}
